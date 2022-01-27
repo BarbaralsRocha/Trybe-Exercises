f4599d64e2106a9a2783a103dea481c30753e529
BarbaraRocha <barbaraluizasr@gmail.com>
2022-01-26 21:28:45 -0300

    11.1

--- Modulo02-FrontEnd/Introducao/my-pokedex/src/App.js
+++ Modulo02-FrontEnd/Introducao/my-pokedex/src/App.js
@@ -3,16 +3,65 @@ import './App.css';
 import pokemons from './data';
 import Pokemon from './pokemon';
 
+//exibir um pokemon de cada vez => ok
+//colocar um botao => ok
+// colocar evento no botao para ir para o proximo => ok
+//fazer verificacao se esta no ultimo => ok
+
+//
+
+//dois botoes contendo os tipos Fire e Psychic
+//quando apertar o botao fire, circular somente os pokemons daquele tipo
+//quando apertar o botao Psychic, circular somente os pokemons daquele tipo
+//um dos botoes ja deve ser inicializado no set.states
+
+
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      index: 0
+    }
+    this.handleClick = this.handleClick.bind(this)
+    this.handleClickFire = this.handleClickFire.bind(this)
+    this.handleClickPsychic = this.handleClickPsychic.bind(this)
+  }
+
+  handleClickFire() {
+    const fire = pokemons.filter((pokemon) => pokemon.type === 'Fire').id
+    pokemons = fire;
+    return this.handleClick
+  }
+
+  handleClick() {
+
+
+    if (this.state.index === pokemons.length - 1) {
+      return this.setState({
+        index: 0
+      })
+    }
+    
+    this.setState((estadoAnterior, _props) => ({
+      index: estadoAnterior.index + 1,
+    }))
+
+  }
+
+
+
+  handleClickPsychic() {
+    return pokemons.filter((pokemon) => pokemon.type === 'Psychic')
+  }
+
   render() {
     return (
       <div className="App">
         <h1> Pokedex </h1>
-        {
-          pokemons.map((pokemonInfo) => {
-            return <Pokemon key={pokemonInfo.id} pokemons={pokemonInfo}/>
-          })
-        }
+        <Pokemon key={pokemons[this.state.index].id} pokemons={pokemons[this.state.index]} />
+        <button onClick={this.handleClick}>Próximo Pokémon</button>
+        <button onClick={this.handleClickFire}>Fire</button>
+        <button onClick={this.handleClickPsychic}>Psychic</button>
       </div>
     );
   }
